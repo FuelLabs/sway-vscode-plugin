d1d055c1d887ffe0de642225a7897ce03f4ab705
alex-matthe <87020692+alex-matthe@users.noreply.github.com>
2022-02-27 19:22:29 +0200

    feat: add code formatter (#24)
    * Add formatter
    
    * Update package-lock.json
    
    * Narrow down the number of scanned files
    
    * Add Github Action workflow file
    
    * Format extension file
    
    * Fix workflow file
    
    * Fix workflow file
    
    * Update .github/workflows/format.yml
    
    Co-authored-by: John Adler <adlerjohn@users.noreply.github.com>
    
    Co-authored-by: John Adler <adlerjohn@users.noreply.github.com>

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,125 +1,132 @@
-import { workspace, ExtensionContext, ExtensionMode, WorkspaceConfiguration } from 'vscode'
+import {
+  workspace,
+  ExtensionContext,
+  ExtensionMode,
+  WorkspaceConfiguration,
+} from "vscode";
 
 import {
-	Executable,
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
-} from 'vscode-languageclient/node'
+  Executable,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
 
-let client: LanguageClient
+let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	client = new LanguageClient(
-		'forc',
-		'Sway',
-		getServerOptions(context),
-		getClientOptions()
-	)
+  client = new LanguageClient(
+    "forc",
+    "Sway",
+    getServerOptions(context),
+    getClientOptions()
+  );
 
-	// Start the client. This will also launch the server
-	console.log("Starting Client and Server")
-	client.start()
+  // Start the client. This will also launch the server
+  console.log("Starting Client and Server");
+  client.start();
 
-	client.onReady().then(_ => {
-		console.log("Client has Connected to the Server successfully!")
-	})
+  client.onReady().then((_) => {
+    console.log("Client has Connected to the Server successfully!");
+  });
 }
 
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined
-	}
-	return client.stop()
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
 }
 
-
 function getServerOptions(context: ExtensionContext): ServerOptions {
-	const serverExecutable: Executable = {
-		command: 'forc',
-		args: ['lsp'],
-		options: {
-			shell: true,
-		}
-	}
+  const serverExecutable: Executable = {
+    command: "forc",
+    args: ["lsp"],
+    options: {
+      shell: true,
+    },
+  };
 
-	const devServerOptions: ServerOptions = {
-		run: serverExecutable,
-		debug: serverExecutable,
-		transport: TransportKind.stdio,
-	}
+  const devServerOptions: ServerOptions = {
+    run: serverExecutable,
+    debug: serverExecutable,
+    transport: TransportKind.stdio,
+  };
 
-	switch (context.extensionMode) {
-		case ExtensionMode.Development:
-		case ExtensionMode.Test:
-			return devServerOptions
+  switch (context.extensionMode) {
+    case ExtensionMode.Development:
+    case ExtensionMode.Test:
+      return devServerOptions;
 
-		default:
-			// TODO: for production we need to be able to install the Language Server
-			return devServerOptions
-	}
+    default:
+      // TODO: for production we need to be able to install the Language Server
+      return devServerOptions;
+  }
 }
 
 function getClientOptions(): LanguageClientOptions {
-	// Options to control the language client
-	const clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [
-			{ scheme: 'file', language: 'sway' },
-			{ scheme: 'untitled', language: 'sway' },
-		],
-		synchronize: {
-			// Notify the server about file changes to *.sw files contained in the workspace
-			fileEvents: [
-				workspace.createFileSystemWatcher('**/.sw'),
-				workspace.createFileSystemWatcher("**/*.sw"),
-			]
-		},
-		initializationOptions: {
-			...getSwayConfigOptions()
-		}
-	}
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [
+      { scheme: "file", language: "sway" },
+      { scheme: "untitled", language: "sway" },
+    ],
+    synchronize: {
+      // Notify the server about file changes to *.sw files contained in the workspace
+      fileEvents: [
+        workspace.createFileSystemWatcher("**/.sw"),
+        workspace.createFileSystemWatcher("**/*.sw"),
+      ],
+    },
+    initializationOptions: {
+      ...getSwayConfigOptions(),
+    },
+  };
 
-	return clientOptions
+  return clientOptions;
 }
 
 function getSwayConfigOptions(): SwayConfig {
-	const swayFormatOptions = getSwayFormattingOptions().format;
+  const swayFormatOptions = getSwayFormattingOptions().format;
 
-	const defaultSwayConfig: SwayConfig = {
-		alignFields: true,
-		tabSize: 4
-	};
+  const defaultSwayConfig: SwayConfig = {
+    alignFields: true,
+    tabSize: 4,
+  };
 
-	if (swayFormatOptions) {
-		return {
-			alignFields: swayFormatOptions.hasOwnProperty('alignFields') ? swayFormatOptions.alignFields : defaultSwayConfig.alignFields,
-			tabSize: swayFormatOptions.hasOwnProperty('tabSize') ? swayFormatOptions.tabSize : defaultSwayConfig.tabSize,
-		}
-	} else {
-		return defaultSwayConfig
-	}
-
+  if (swayFormatOptions) {
+    return {
+      alignFields: swayFormatOptions.hasOwnProperty("alignFields")
+        ? swayFormatOptions.alignFields
+        : defaultSwayConfig.alignFields,
+      tabSize: swayFormatOptions.hasOwnProperty("tabSize")
+        ? swayFormatOptions.tabSize
+        : defaultSwayConfig.tabSize,
+    };
+  } else {
+    return defaultSwayConfig;
+  }
 }
 
 function getSwayFormattingOptions(): WorkspaceConfiguration | null {
-	const swayOptions = workspace.getConfiguration("sway");
-	const swayOptionsBracket = workspace.getConfiguration("[sway]");
+  const swayOptions = workspace.getConfiguration("sway");
+  const swayOptionsBracket = workspace.getConfiguration("[sway]");
 
-	if (swayOptions && swayOptions.format) {
-		if (swayOptionsBracket && swayOptionsBracket.format) {
-			return Object.assign({}, swayOptions, swayOptionsBracket);
-		}
-		return swayOptions
-	} else if (swayOptionsBracket && swayOptionsBracket.format) {
-		return swayOptionsBracket
-	} else {
-		return null
-	}
+  if (swayOptions && swayOptions.format) {
+    if (swayOptionsBracket && swayOptionsBracket.format) {
+      return Object.assign({}, swayOptions, swayOptionsBracket);
+    }
+    return swayOptions;
+  } else if (swayOptionsBracket && swayOptionsBracket.format) {
+    return swayOptionsBracket;
+  } else {
+    return null;
+  }
 }
 
 type SwayConfig = {
-	alignFields: boolean,
-	tabSize: number,
-}
+  alignFields: boolean;
+  tabSize: number;
+};
