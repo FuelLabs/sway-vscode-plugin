de64f8bca517b0fe96a8f77fe8ac4c57623721a7
Sophie <sophiedankel@gmail.com>
2022-07-27 19:09:12 -0700

    Runnables interfae

--- client/src/code_lens/provider.ts
+++ client/src/code_lens/provider.ts
@@ -2,9 +2,11 @@ import {
   CodeLens,
   CodeLensProvider,
   Command,
-  Range,
+  // Range,
   TextDocument,
 } from 'vscode';
+import { getRunnables } from '../interface/getRunnables';
+import { log } from '../util';
 
 export class SwayCodeLensProvider implements CodeLensProvider {
   async provideCodeLenses(document: TextDocument): Promise<CodeLens[]> {
@@ -12,11 +14,12 @@ export class SwayCodeLensProvider implements CodeLensProvider {
     let forcRun: Command = {
       command: 'sway.runScript',
       title: '▶\u{fe0e} Run',
+      
       tooltip: 'Run the Sway script with Forc',
     };
 
     // TODO: Get range of runnable function(s) from server
-    const runnableRanges = [new Range(0, 0, 0l, 0)]
+    const runnableRanges = await getRunnables(document.uri.path);// [new Range(0, 0, 0, 0)]
     const forcRunLenses = runnableRanges.map(range => new CodeLens(range, forcRun));
 
     return forcRunLenses;
