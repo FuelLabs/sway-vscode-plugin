2f66828a1e0909d8f0275b6e7081f586a30c6412
Sophie <sophiedankel@gmail.com>
2022-08-12 17:01:10 -0700

    missing import

--- client/src/commands/forcBuild.ts
+++ client/src/commands/forcBuild.ts
@@ -1,12 +1,13 @@
 import { exec } from 'child_process';
 import { window } from 'vscode';
+import { log } from '../util';
 
 export default function forcBuild(forcDir: string) {
   exec(`cd ${forcDir} && forc run`, (error, stdout, _stderr) => {
     if (error) {
       window.showInformationMessage(`Failed: see output console for error`);
       log.error(stdout);
-    } else {
+    } else {g
       window.showInformationMessage(`Successfully built sway program`);
       log.info(stdout);
     }
