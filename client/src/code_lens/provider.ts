f311405d822e50b899800b29c41e1c98405d72ce
Sophie <sophiedankel@gmail.com>
2022-08-15 18:28:57 -0700

    Remove build button

--- client/src/code_lens/provider.ts
+++ client/src/code_lens/provider.ts
@@ -2,21 +2,15 @@ import { CodeLens, CodeLensProvider, Command, TextDocument } from 'vscode';
 import { getRunnables, Runnable } from '../interface/getRunnables';
 
 export class SwayCodeLensProvider implements CodeLensProvider {
-  async provideCodeLenses(document: TextDocument): Promise<CodeLens[]> {
+  async provideCodeLenses(_document: TextDocument): Promise<CodeLens[]> {
     const runnables: Runnable[] = await getRunnables();
     const forcRun: Command = {
       command: 'sway.runScript',
       title: '▶\u{fe0e} Run',
       tooltip: 'Run the Sway program with Forc',
     };
-    const forcBuild: Command = {
-      command: 'sway.forcBuild',
-      title: '▶\u{fe0e} Build',
-      tooltip: 'Build the Sway program with Forc',
-    };
-    const runnableLenses = runnables.map(([range, programType]: Runnable) => {
-      const command = programType === 'script' ? forcRun : forcBuild;
-      return new CodeLens(range, command);
+    const runnableLenses = runnables.map(([range]: Runnable) => {
+      return new CodeLens(range, forcRun);
     });
 
     return runnableLenses;
