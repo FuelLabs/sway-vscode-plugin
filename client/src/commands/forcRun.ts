93ebe4fb016e5c7b7dd7d49ec1501454a89ad6ea
Sophie <sophiedankel@gmail.com>
2023-01-20 18:01:30 -0800

    Add command for running tests

--- client/src/commands/forcRun.ts
+++ client/src/commands/forcRun.ts
@@ -1,15 +1,5 @@
-import { exec } from 'child_process';
-import { window } from 'vscode';
 import { log } from '../util';
 
 export default function forcRun(forcDir: string) {
-  exec(`cd ${forcDir} && forc run --unsigned`, (error, stdout, stderr) => {
-    if (error) {
-      window.showInformationMessage(`Failed: see output console for error`);
-      log.error(stderr);
-    } else {
-      window.showInformationMessage(`Successfully ran script`);
-      log.info(stdout);
-    }
-  });
+  log.terminal(`cd ${forcDir} && forc run --unsigned`);
 }
