d83ba45cd5c5ea45ed1c3a874f7623ef9d2a181b
Sophie <sophiedankel@gmail.com>
2023-03-20 14:58:59 -0700

    Check and install server executable

---
+++ client/src/commands/installServer.ts
@@ -0,0 +1,5 @@
+import { Terminal } from '../util';
+
+export default function installServer() {
+  Terminal.Sway.execute(`curl --proto '=https' --tlsv1.2 -sSf https://install.fuel.network/fuelup-init.sh | sh`);
+}
