41bb4671c3d2c7ae0222abcdf2cbde9b2d562ab2
Sophie Dankel <47993817+sdankel@users.noreply.github.com>
2024-04-16 23:36:48 -0700

    feat: Allow overriding the path to forc-lsp executable (#174)
    * Add configuration to let users select their forc-lsp
    
    Fixes #167
    
    By default the field is empty and it is only useful to advanced users.
    
    The runtime will use the field to try to boot the lsp but in case the
    configuration is invalid it will fallback to finding the forc-lsp in $PATH
    
    * Allow overriding the path to forc-lsp executable
    
    * prettier
    
    * update reload opts
    
    * fix typo
    
    ---------
    
    Co-authored-by: Cesar Rodas <cesar@rodasm.com.py>

--- client/src/main.ts
+++ client/src/main.ts
@@ -31,7 +31,10 @@ export async function activate(context: ExtensionContext) {
   );
 
   try {
-    const client = createClient(getClientOptions(), await getServerOptions());
+    const client = createClient(
+      getClientOptions(),
+      await getServerOptions(config)
+    );
 
     // Start the client. This will also launch the server
     await client.start();
@@ -53,35 +56,62 @@ export function deactivate(): Thenable<void> | undefined {
   return client.stop();
 }
 
-async function getServerOptions(): Promise<lc.ServerOptions> {
-  // Look for the executable in FUELUP_HOME if it exists, otherwise look for it in the PATH.
-  const executable = process.env.FUELUP_HOME
+async function getServerOptions(config: Config): Promise<lc.ServerOptions> {
+  // Look for the default executable in FUELUP_HOME if it exists, otherwise look for it in the PATH.
+  const defaultExecutable = process.env.FUELUP_HOME
     ? `${process.env.FUELUP_HOME}/bin/${LSP_EXECUTABLE_NAME}`
     : LSP_EXECUTABLE_NAME;
+
+  // Use the settings override path if provided, otherwise use the default executable.
+  const settingsExecutable = config.binPath;
+  const executable = settingsExecutable || defaultExecutable;
 
   // Check if the executable exists.
   try {
     let version = await promisify(exec)(`${executable} --version`);
     log.info(`Server executable version: ${version.stdout.trim()}`);
   } catch (error) {
-    window
-      .showErrorMessage(
-        'The Sway Language Server is not installed. Would you like to install it?',
-        'Yes',
-        'No'
-      )
-      .then(async selection => {
-        if (selection === 'Yes') {
-          window.showInformationMessage(
-            'Follow the instructions in the terminal to install the Sway Language Server, then reload the window.'
-          );
-          commands.executeCommand('sway.installServer');
-        }
-      });
-    throw Error(`Missing executable: ${LSP_EXECUTABLE_NAME}\
+    if (!!settingsExecutable) {
+      const updateMessage =
+        'Update the setting "sway-lsp.diagnostic.binPath" either to a valid path to a forc-lsp executable, or leave it empty to use the executable to which your $PATH resolves.';
+      window
+        .showErrorMessage(
+          'The Sway Language Server is not installed at the path defined in your Extension Settings.',
+          'Edit Setting',
+          'Later'
+        )
+        .then(async selection => {
+          if (selection === 'Edit Setting') {
+            window.showInformationMessage(updateMessage);
+            commands.executeCommand(
+              'workbench.action.openWorkspaceSettings',
+              'sway-lsp.diagnostic.binPath'
+            );
+          }
+        });
+      throw Error(`Missing executable: ${LSP_EXECUTABLE_NAME}\
+        \n\nThe VSCode setting "sway-lsp.diagnostic.binPath" is set to an invalid path: "${settingsExecutable}"
+        \n${updateMessage}`);
+    } else {
+      window
+        .showErrorMessage(
+          'The Sway Language Server is not installed. Would you like to install it?',
+          'Yes',
+          'No'
+        )
+        .then(async selection => {
+          if (selection === 'Yes') {
+            window.showInformationMessage(
+              'Follow the instructions in the terminal to install the Sway Language Server, then reload the window.'
+            );
+            commands.executeCommand('sway.installServer');
+          }
+        });
+      throw Error(`Missing executable: ${LSP_EXECUTABLE_NAME}\
         \n\nYou may need to install the fuel toolchain or add ${process.env.HOME}/.fuelup/bin your path. Try running:\
         \n\ncurl --proto '=https' --tlsv1.2 -sSf https://install.fuel.network/fuelup-init.sh | sh\
         \n\nOr read about fuelup for more information: https://github.com/FuelLabs/fuelup\n`);
+    }
   }
 
   // Get the full path to the server executable.
