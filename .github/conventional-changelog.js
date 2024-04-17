b9e84aed444ce50d7efe2b211ef3cf7ad33e3fcc
Sophie <sophiedankel@gmail.com>
2024-04-17 12:26:52 -0700

    conventional changelog config

---
+++ .github/conventional-changelog.js
@@ -0,0 +1,16 @@
+'use strict'
+const config = require('conventional-changelog-conventionalcommits');
+
+module.exports = config({
+    "types": [
+        {"type": "feat", "section": "Features"},
+        {"type": "fix", "section": "Bug Fixes"},
+        {"type": "chore", "hidden": true},
+        {"type": "docs", "hidden": true},
+        {"type": "style", "hidden": true},
+        {"type": "refactor", "hidden": true},
+        {"type": "perf", "hidden": true},
+        {"type": "test", "hidden": true},
+        {"type": "revert", "hidden": true}
+    ]
+})
